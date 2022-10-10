21aca64abcae1a5e3d19857c0aecbe20d6d9aebe
phikhanhcr <sykhanhsky@gmail.com>
2022-10-10 13:59:17 +0700

    oke

--- assets/scripts/env.js
+++ assets/scripts/env.js
@@ -1,2 +1,2 @@
 const backend_api_url = 'https://api.stg.bituclub.com/'
-const landing_mod_upload_api_url = '$LANDING_MOD_UPLOAD_API_URL'
+const landing_mod_upload_api_url = 'https://upload.bitu.asia'
